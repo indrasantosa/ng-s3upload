22a461b0232506a456e49e5ca377ff93ece7e36d
Indra Santosa <indrasantosa90@gmail.com>
2019-07-31 00:53:02 +0800

    update http implementation

--- src/ng-s3upload/services/s3-uploader.js
+++ src/ng-s3upload/services/s3-uploader.js
@@ -1,30 +1,46 @@
-angular.module('ngS3upload.services').
-  service('S3Uploader', ['$http', '$q', '$window', function ($http, $q, $window) {
+angular.module('ngS3upload.services').service('S3Uploader', [
+  '$http',
+  '$q',
+  '$window',
+  function($http, $q, $window) {
     this.uploads = 0;
     var self = this;
 
-    this.getUploadOptions = function (uri) {
+    this.getUploadOptions = function(uri) {
       var deferred = $q.defer();
-      $http.get(uri).
-        success(function (response, status) {
+      $http
+        .get(uri)
+        .then(function(response, status) {
           deferred.resolve(response);
-        }).error(function (error, status) {
+        })
+        .catch(function(error, status) {
           deferred.reject(error);
         });
 
       return deferred.promise;
     };
 
-    this.randomString = function (length) {
-      var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.randomString = function(length) {
+      var chars =
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
       var result = '';
-      for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+      for (var i = length; i > 0; --i)
+        result += chars[Math.round(Math.random() * (chars.length - 1))];
 
       return result;
     };
 
-
-    this.upload = function (scope, uri, key, acl, type, accessKey, policy, signature, file) {
+    this.upload = function(
+      scope,
+      uri,
+      key,
+      acl,
+      type,
+      accessKey,
+      policy,
+      signature,
+      file
+    ) {
       var deferred = $q.defer();
       scope.attempt = true;
 
@@ -35,40 +51,40 @@ angular.module('ngS3upload.services').
       fd.append('AWSAccessKeyId', accessKey);
       fd.append('policy', policy);
       fd.append('signature', signature);
-      fd.append("file", file);
+      fd.append('file', file);
 
       var xhr = new XMLHttpRequest();
-      xhr.upload.addEventListener("progress", uploadProgress, false);
-      xhr.addEventListener("load", uploadComplete, false);
-      xhr.addEventListener("error", uploadFailed, false);
-      xhr.addEventListener("abort", uploadCanceled, false);
+      xhr.upload.addEventListener('progress', uploadProgress, false);
+      xhr.addEventListener('load', uploadComplete, false);
+      xhr.addEventListener('error', uploadFailed, false);
+      xhr.addEventListener('abort', uploadCanceled, false);
       scope.$emit('s3upload:start', xhr);
 
       // Define event handlers
       function uploadProgress(e) {
-        scope.$apply(function () {
+        scope.$apply(function() {
           if (e.lengthComputable) {
-            scope.progress = Math.round(e.loaded * 100 / e.total);
+            scope.progress = Math.round((e.loaded * 100) / e.total);
           } else {
             scope.progress = 'unable to compute';
           }
-          var msg = {type: 'progress', value: scope.progress};
+          var msg = { type: 'progress', value: scope.progress };
           scope.$emit('s3upload:progress', msg);
           if (typeof deferred.notify === 'function') {
             deferred.notify(msg);
           }
-
         });
       }
       function uploadComplete(e) {
         var xhr = e.srcElement || e.target;
-        scope.$apply(function () {
+        scope.$apply(function() {
           self.uploads--;
           scope.uploading = false;
-          if (xhr.status === 204) { // successful upload
+          if (xhr.status === 204) {
+            // successful upload
             scope.success = true;
             deferred.resolve(xhr);
-            scope.$emit('s3upload:success', xhr, {path: uri + key});
+            scope.$emit('s3upload:success', xhr, { path: uri + key });
           } else {
             scope.success = false;
             deferred.reject(xhr);
@@ -78,7 +94,7 @@ angular.module('ngS3upload.services').
       }
       function uploadFailed(e) {
         var xhr = e.srcElement || e.target;
-        scope.$apply(function () {
+        scope.$apply(function() {
           self.uploads--;
           scope.uploading = false;
           scope.success = false;
@@ -88,7 +104,7 @@ angular.module('ngS3upload.services').
       }
       function uploadCanceled(e) {
         var xhr = e.srcElement || e.target;
-        scope.$apply(function () {
+        scope.$apply(function() {
           self.uploads--;
           scope.uploading = false;
           scope.success = false;
@@ -106,7 +122,8 @@ angular.module('ngS3upload.services').
       return deferred.promise;
     };
 
-    this.isUploading = function () {
+    this.isUploading = function() {
       return this.uploads > 0;
     };
-  }]);
+  }
+]);
